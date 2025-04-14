82c76572cc7ec601d58642cdcd890974f73a0602
fabricyoc <fazin_c@hotmail.com>
2025-04-14 08:52:21 -0300

    fix: responsividade em Arquivos

--- src/pages/Arquivos/index.jsx
+++ src/pages/Arquivos/index.jsx
@@ -1,9 +1,13 @@
-import CardsFiles from '../../components/CardsFiles';
 import styles from './Arquivos.module.css';
+import CardsFiles from '../../components/CardsFiles';
+import ScrollToTopButton from '../../components/ScrollToTopButton';
 
 function Arquivos() {
   return (
     <section className={styles.arquivos}>
+      <span className={styles.scroll}>
+        <ScrollToTopButton />
+      </span>
       <h2>Arquivos e documentos</h2>
       <CardsFiles />
     </section>
