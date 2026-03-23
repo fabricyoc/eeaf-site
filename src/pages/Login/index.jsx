f5fa4509b0d190bcbbd391b2974dac4cb9fe4f84
fabricyoc <fazin_c@hotmail.com>
2026-03-23 12:00:37 -0300

    fix: login icon

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,12 +3,12 @@ import styles from './Login.module.css';
 
 function Login() {
   return (
-      <section className={styles.cabecalhoLogin}>
-        <img src="/logo.png" alt="" />
-        <h2>SIGEF</h2>
-        <p>Sistema de Gestão Escolar e Feiras de Ciências</p>
+    <section className={styles.cabecalhoLogin}>
+      <img src="/sigef/sigef.png" alt="" />
+      <h2>SIGEF</h2>
+      <p>Sistema de Gestão Escolar e Feiras de Ciências</p>
       <FormLogin />
-      </section>
+    </section>
   );
 }
 
